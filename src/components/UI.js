84f5ce5d7758042ec0cf458490fb3c612dcc3e4b
jamcry <jamcry@hotmail.com>
2019-07-14 01:32:45 +0300

    Implement adding new todo feature

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,12 +1,29 @@
-import Project from './Project';
-import Todo from './Todo';
+import Project from "./Project";
+import Todo from "./Todo";
 
 class UI {
   constructor() {
-    this.projects = document.querySelector('.projects');
-    this.todos = document.querySelector('.todos');
+    this.projects = document.querySelector(".projects");
+    this.todos = document.querySelector(".todos");
+    this.contentTitle = document.querySelector("#content-title");
+    this.currentProject = null;
+
+    this.todoForm = document.querySelector(".new-todo");
+    this.todoFormDetails = document.querySelector(".new-todo-details");
+
+    // Expand the form when focused on it
+    this.todoForm.addEventListener("focusin", e => {
+      this.todoFormDetails.style.display = "block";
+    });
+
+    this.todoForm.addEventListener("submit", e => {
+      this.todoFormDetails.style.display = "none";
+      this.handleTodoFormSubmit(e);
+    });
+
     this.handleProjectClick = this.handleProjectClick.bind(this);
     this.handleTodoClick = this.handleTodoClick.bind(this);
+    this.handleTodoFormSubmit = this.handleTodoFormSubmit.bind(this);
   }
 
   // Creates and renders a new Project
@@ -24,13 +41,17 @@ class UI {
   }
 
   /*
-   EVENT HANDLERS
+    EVENT HANDLERS
   */
 
+  /* Click handlers */
+
   handleProjectClick(project) {
-    if(project.todoCount > 0) {
+    if (project.todoCount > 0) {
       // Clear the previously rendered todos
-      this.todos.innerHTML = '';
+      this.todos.innerHTML = "";
+      this.currentProject = project;
+      this.contentTitle.textContent = this.currentProject.title;
       project.todos.forEach(todo => this.render(todo));
     }
   }
@@ -39,10 +60,27 @@ class UI {
     alert(`You have clicked on ${todo.title}`);
   }
 
+  /* Submit handlers */
+  handleTodoFormSubmit(e) {
+    e.preventDefault();
+    const todoForm = e.target;
+    const todoTitle = todoForm.querySelector("#todo-title").value;
+    const todoDesc = todoForm.querySelector("#todo-description").value;
+    const todoDue = todoForm.querySelector("#todo-due").value;
+    const todoImportant = todoForm.querySelector("#todo-important").checked;
+    const todoPriority = todoImportant ? "HIGH" : "NORMAL";
+    this.todoForm.reset();
+    this.currentProject.newTodo(todoTitle, todoDesc, todoDue, todoPriority);
+    // Render the new (last) item
+    this.render(
+      this.currentProject.todos[this.currentProject.todos.length - 1]
+    );
+  }
+
   // Renders the given item
   render(item) {
     if (item instanceof Project) {
-      item.appendTo(this.projects, this.handleProjectClick)
+      item.appendTo(this.projects, this.handleProjectClick);
     } else if (item instanceof Todo) {
       item.appendTo(this.todos, this.handleTodoClick);
     } else {
@@ -52,7 +90,7 @@ class UI {
 
   // Render project's todos
   renderProject(project) {
-    if(project.todoCount > 0) { 
+    if (project.todoCount > 0) {
       project.todos.forEach(todo => this.render(todo));
     }
   }
